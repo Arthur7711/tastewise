c0a25c646c1d0f13cc93246033d5103745e14a6c
Arthur <newartroll11@gmail.com>
2021-12-07 13:53:53 +0400

    feat:(home page)

--- components/header/Header.js
+++ components/header/Header.js
@@ -8,31 +8,37 @@ import SimpleBTN from "../simpleBTN/SimpleBTN";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div style={{ height: 100, display: "flex", alignItems: "center" }}>
-        <Image src={logo} alt="logo" />
+      <div className={styles.left}>
+        <div style={{ height: 100, display: "flex", alignItems: "center" }}>
+          <Image src={logo} alt="logo" />
+        </div>
+        <MySelect values={["Solutions", 2, 3]} />
+        <MySelect values={["Resources", 2, 3]} />
+        <MySelect values={["About", 2, 3]} />
+        <p>Pricing</p>
       </div>
-      <MySelect values={["Solutions", 2, 3]} />
-      <MySelect values={["Resources", 2, 3]} />
-      <MySelect values={["About", 2, 3]} />
-      <p>Pricing</p>
-      <div style={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
-        <svg
-          style={{ width: "20px", height: "20px" }}
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      <div className={styles.right}>
+        <div
+          style={{ display: "flex", alignItems: "center", cursor: "pointer" }}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
-        <p>Log in</p>
+          <svg
+            style={{ width: "20px", height: "20px" }}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+            />
+          </svg>
+          <p>Log in</p>
+        </div>
+        <SimpleBTN title="Get started" />
       </div>
-      <SimpleBTN title="Get started" />
     </header>
   );
 }

dbe5a22780480c4abac5b001685d04744c99fd58
Arthur <newartroll11@gmail.com>
2021-12-10 18:13:11 +0400

    fix

--- components/slideItem/SlideItem.js
+++ components/slideItem/SlideItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Slideitem.module.css";
+import styles from "./SlideItem.module.css";
 import Image from "next/image";
 
 const SlideItem = ({ logo, text, avatar, name, description }) => {
